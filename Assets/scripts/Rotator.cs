349da34dabf95c1ba76d2b7238924491ccd4455a
Gidi Rabi <118205453+GidiRabi@users.noreply.github.com>
2024-12-08 20:44:56 +0200

    changed rotator

--- Assets/scripts/Rotator.cs
+++ Assets/scripts/Rotator.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class Rotator : MonoBehaviour
 {
 
-
+    [Header("Rotation Settings")]
+    [SerializeField]
     public Vector3 rotationSpeed = new Vector3(0, 100, 0);
 
     void Update()
